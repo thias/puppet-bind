906592e16720188d2c87626be892770614261d45
Matthias Saou <thias@marmotte.net>
2011-06-05 23:17:34 +0200

    New bind module with a simple server class and simple yet effective server::conf definition.

---
+++ manifests/server.pp
@@ -0,0 +1,46 @@
+# ISC BIND server module.
+#
+# Sample Usage :
+#    include bind::server
+# or
+#    class bind::server {
+#        chroot        => false,
+#        bindpkgprefix => "bind97",
+#    }
+#
+class bind::server (
+    $chroot = true,
+    # For RHEL5 you might want to use "bind97"
+    $bindpkgprefix = "bind"
+) {
+
+    # Main package and service it provides
+    $bindserverpkgname = $chroot ? {
+        true  => "${bindpkgprefix}-chroot",
+        false => "${bindpkgprefix}",
+    }
+    package { $bindserverpkgname: ensure => installed }
+    service { "named":
+        require   => Package[$bindserverpkgname],
+        hasstatus => true,
+        enable    => true,
+        ensure    => running,
+        restart   => "/sbin/service named reload",
+    }
+
+    # We want a nice log file which the package doesn't provide a location for
+    $bindlogdir = $chroot ? {
+        true  => "/var/named/chroot/var/log/named",
+        false => "/var/log/named",
+    }
+    file { $bindlogdir:
+        require => Package[$bindserverpkgname],
+        ensure  => directory,
+        owner   => "root",
+        group   => "named",
+        mode    => 0770,
+        seltype => "named_log_t",
+    }
+
+}
+
---
+++ manifests/server/conf.pp
@@ -0,0 +1,50 @@
+# ISC BIND server template-based configuration definition.
+#
+# Sample Usage :
+#    bind::server::conf { '/etc/named.conf':
+#        acls => {
+#            'rfc1918' => [ '10/8', '172.16/12', '192.168/16' ],
+#        },
+#        masters => {
+#            'mymasters' => [ '192.0.2.1', '198.51.100.1' ],
+#        },
+#        zones => {
+#            'example.com' => [
+#                'type master',
+#                'file "example.com"',
+#            ],
+#            'example.org' => [
+#                'type slave',
+#                'file "slaves/example.org"',
+#                'masters { mymasters; }',
+#            ],
+#        }
+#    }
+#
+define bind::server::conf (
+    $acls = false,
+    $masters = false,
+    $listen_on_port = "53",
+    $listen_on_addr = [ "127.0.0.1" ],
+    $listen_on_v6_port = "53",
+    $listen_on_v6_addr = [ "::1" ],
+    $directory = "/var/named",
+    $version = false,
+    $dump_file = "/var/named/data/cache_dump.db",
+    $statistics_file = "/var/named/data/named_stats.txt",
+    $memstatistics_file = "/var/named/data/named_mem_stats.txt",
+    $allow_query = [ "localhost" ],
+    $recursion = "yes",
+    $dnssec_enable = "yes",
+    $dnssec_validation = "yes",
+    $dnssec_lookaside = "auto",
+    $zones = false
+) {
+
+    file { $title:
+        notify => Service["named"],
+        content => template("bind/named.conf.erb"),
+    }
+
+}
+
