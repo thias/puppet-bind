91315635416906a788aa8550c5d4ca13d4282b44
Sebastian Cole <sebastian.paris@gmail.com>
2013-04-27 16:43:09 +1000

    expanded bind::server::file tests
    realised it's not that easy to test file contents when you specify
    source instead of content => template('')
    
    following testing examples from
    http://genuxation.com/wiki/index.php/RSpec_tests_for_puppet_modules

--- spec/defines/bind_server_file_spec.rb
+++ spec/defines/bind_server_file_spec.rb
@@ -1,8 +1,10 @@
 require 'spec_helper'
 
 describe 'bind::server::file' do
+	let(:title) { 'example.com' }
 	let (:params) {{
-		:title => 'example.com',
 		:source => 'puppet:///modules/bind/named.empty'
 	}}
+
+	it { should contain_file('/var/named/example.com') }
 end
