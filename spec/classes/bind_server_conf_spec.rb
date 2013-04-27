99c894a6af2addcab6d2979d09d84eb826c32f54
Sebastian Cole <sebastian.paris@gmail.com>
2013-04-27 15:37:58 +1000

    Replicating tests/server/conf.pp into rspec format
    starting small by replicating the params passed into the defined type,
    form here I'll expand on the class expectations.

---
+++ spec/classes/bind_server_conf_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe 'bind::server::conf' do
+	let (:params) { {
+		:title => '/etc/named.conf', 
+		:acls => { 
+			'rfc1918' => [ '10/8', '172.16/12', '192.168/16' ] 
+		},
+		:masters => {
+			'mymasters' => ['192.0.2.1', '198.51.100.1']
+		},
+		:zones => {
+			'example.com' => [
+				'type master',
+				'file "example.com"',
+			],
+			'example.org' => [
+				'type slave',
+				'file "slaves/example.org"',
+				'masters { mymasters; }',
+			],
+		},
+		:includes => [
+			'/etc/myzones.conf',
+		],
+	} }
+
+end
