d68f088e7c1dc5c465a44a73901cf91ee8563e3f
John Bond <john.r.bond@gmail.com>
2014-02-21 14:47:21 +0100

    * we dont want rfc1918 zone or root hints unless we are recursive * ensure that the named dir exists * add bind group * change some defaults and only add dnssec resolver functions if we * recurse
    bla

--- manifests/server/file.pp
+++ manifests/server/file.pp
@@ -31,7 +31,7 @@
 define bind::server::file (
   $zonedir     = '/var/named',
   $owner       = 'root',
-  $group       = undef,
+  $group       = $::bind::params::bindgroup,
   $mode        = '0640',
   $dirmode     = '0750',
   $source      = undef,
