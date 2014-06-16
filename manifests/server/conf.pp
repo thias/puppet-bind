4dda685635cb23d13421560b44484b8298502846
Herwig Bogaert <herwig@gmail.com>
2014-06-16 11:00:58 +0200

    improved support for debian

--- manifests/server/conf.pp
+++ manifests/server/conf.pp
@@ -62,6 +62,8 @@
 #   and the value is an array of config lines. Default: empty
 #  $includes:
 #   Array of absolute paths to named.conf include files. Default: empty
+#   on Debian systems,  consider adding the 1918 zones here, if they are not used in your
+#   organisation. (/etc/bind/zones.rfc1918)
 #
 # Sample Usage :
 #  bind::server::conf { '/etc/named.conf':
@@ -82,7 +84,7 @@
 #        'masters { mymasters; }',
 #      ],
 #    }
-#    keys                 => { 
+#    keys                 => {
 #      'example.org-tsig' => [
 #        'algorithm hmac-md5',
 #        'secret "aaabbbcccddd"',
@@ -98,15 +100,15 @@ define bind::server::conf (
   $listen_on_v6_port      = '53',
   $listen_on_v6_addr      = [ '::1' ],
   $forwarders             = [],
-  $directory              = '/var/named',
+  $directory              = $::bind::params::directory,
   $managed_keys_directory = undef,
   $hostname               = undef,
   $forward                = undef,
   $server_id              = undef,
   $version                = undef,
-  $dump_file              = '/var/named/data/cache_dump.db',
-  $statistics_file        = '/var/named/data/named_stats.txt',
-  $memstatistics_file     = '/var/named/data/named_mem_stats.txt',
+  $dump_file              = $::bind::params::dump_file,
+  $statistics_file        = $::bind::params::statistics_file,
+  $memstatistics_file     = $::bind::params::memstatistics_file,
   $allow_query            = [ 'localhost' ],
   $allow_query_cache      = [],
   $recursion              = 'yes',
