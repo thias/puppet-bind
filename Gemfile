d66d18b62d87f00d663787ff3898bf67d691144f
Kuczyński Konrad <Konrad.Kuczynski@coi.gov.pl>
2017-11-17 14:12:11 +0100

    Couple fixes to achive compatibility described in metadata.json (Puppet 2.x), tests are passing

--- Gemfile
+++ Gemfile
@@ -4,6 +4,6 @@ source "https://rubygems.org"
 # gem "rails"
 gem 'rspec-puppet'
 gem 'rake'
-gem 'puppet'
+gem 'puppet', '~> 2'
 gem 'puppetlabs_spec_helper'
 gem 'puppet-lint'
--- manifests/service.pp
+++ manifests/service.pp
@@ -7,7 +7,7 @@ class bind::service (
 
   if $service_reload {
     Service[$servicename] {
-      restart => "service ${servicename} reload",
+      restart => "/sbin/service ${servicename} reload",
     }
   }
 
@@ -17,5 +17,4 @@ class bind::service (
     hasstatus => true,
     require   => Class['bind::package'],
   }
-  
 }
