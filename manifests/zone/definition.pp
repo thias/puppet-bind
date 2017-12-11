ef0f0eb3fb1ce04dcba71765684f5e2a92599f66
Kuczyński Konrad <Konrad.Kuczynski@coi.gov.pl>
2017-12-11 11:31:14 +0100

    description of zone's definition parametres

--- manifests/zone/definition.pp
+++ manifests/zone/definition.pp
@@ -1,5 +1,35 @@
 # == Define: bind::zone::definition
 # Creates definition and content preamble for DNS zone
+# Define: bind::server::conf
+#
+# Parameters:
+#  $definition_file:
+#   full name of the /etc/named.conf.* file,
+#  $zone_file:
+#   full name of the zone file,
+#  $serial:
+#   version of zone file
+#  $soa_nameserver:
+#   domain name, default: localhost
+#  $soa_contact:
+#   contact to dns admininistrator, default: root.localhost
+#  $ttl:
+#   time to hold data in cache, default: 604800,
+#  $minimum_ttl:
+#   minimut time to hold data in cache, default: 604800,
+#  $refresh:
+#   refresh wait time, default: 604800,
+#  $retry:
+#   retry wait time, default: 86400,
+#  $expire:
+#   time after which data is outdated, default: 2419200,
+#  $zone_type:
+#   wheather zone is master or hint, default: master,
+#  $allow_update:
+#   hostnames allowed to submit dynamic updates, default: undef,
+#  $origin:
+#   name of the domain, default: undef,
+
 define bind::zone::definition (
   $definition_file,
   $zone_file,
--- manifests/zone/record.pp
+++ manifests/zone/record.pp
@@ -1,5 +1,20 @@
 # == Define: bind::zone::record
 # Creates a resource record in DNS zone file
+#
+# Parameters:
+#  $target_file:
+#   full name of the zone file,
+#  $rname:
+#   name of person or role account dealing with this zone, can be alias,
+#  $rdata:
+#   ip or name of server-record,
+#  $rtype:
+#   type of dns record, default: A,
+#  $rclass
+#   zone class, default: IN,
+#  $order:
+#   oreder of concat fragment, default: 99,
+
 define bind::zone::record (
   $target_file,
   $rname,
